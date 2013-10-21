5152b901a41a6267c8c90f91b24d6781822fa081
Allen Johnson <allen@commercekitchen.com>
2013-10-21 10:33:26 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '567b2f589f743eda84b4cb12cdc2248c0c7a015177db37741f57b555d97e752cf36cbc426d9f7033b84cf4255e38a4b58c3fe2df6fdfcd3e60c139ed3d100e46'
